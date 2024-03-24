6e93eb3f06de0bb69a7a43e8cc2a21994f4eb168
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-03-24 15:54:59 -0300

    config(none-ls): new

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,34 +1,34 @@
 return {
-  "nvimtools/none-ls.nvim",
-  keys = {
-    { "<leader>cf",
-      function ()
-       vim.lsp.buf.format({
-          filter = function (client)
-            return client.name == 'null-ls'
-          end
-        })
-      end
-    , desc = "Formatting" },
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local code_actions = null_ls.builtins.code_actions
-    local diagnostics = null_ls.builtins.diagnostics
-    local formatting = null_ls.builtins.formatting
-    local hover = null_ls.builtins.hover
-    local completion = null_ls.builtins.completion
+	"nvimtools/none-ls.nvim",
+  lazy = false,
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				vim.lsp.buf.format({
+					filter = function(client)
+						return client.name == "null-ls"
+					end,
+				})
+			end,
+			desc = "Formatting",
+		},
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
 
-    null_ls.setup({
-      sources = {
-        formatting.stylua.with({
-          filetypes = { "lua" },
-        }),
-        diagnostics.eslint,
-        formatting.prettier.with({
-          filetypes = { "javascript", "typescript" },
-        }),
-      },
-    })
-  end,
+		null_ls.setup({
+			sources = {
+				formatting.stylua,
+        formatting.prettier,
+				require("none-ls.diagnostics.eslint"),
+--				require("none-ls.formatting.eslint"),
+				require("none-ls.code_actions.eslint"),
+			},
+		})
+	end,
 }
