b936d3ea63022d020e28c0e7774fa650e9a49def
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-03-26 23:02:22 -0300

    add new lps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		local opts = {
-			diagnostics = {
-				underline = true,
-				update_in_insert = true,
-				virtual_text = {
-					spacing = 2,
-					source = "if_many",
-					prefix = "●",
-				},
-				severity_sort = true,
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = " ",
-						[vim.diagnostic.severity.WARN] = " ",
-						[vim.diagnostic.severity.HINT] = " ",
-						[vim.diagnostic.severity.INFO] = " ",
-					},
-				},
-			},
-			inlay_hints = { enabled = false },
-			codelens = { enabled = false },
-		}
-
-    vim.diagnostic.config(opts)
-
 		local lspconfig = require("lspconfig")
 
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -55,6 +30,94 @@ return {
 					completeFunctionCalls = true,
 				},
 			},
+		})
+
+		lspconfig.yamlls.setup({
+			capabilities = capabilities,
+			settings = {
+				completions = {
+					completeFunctionCalls = true,
+				},
+			},
+		})
+
+		lspconfig.eslint.setup({
+			on_attach = function(client, bufnr)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					command = "EslintFixAll",
+				})
+			end,
+		})
+
+		lspconfig.sqlls.setup({
+			capabilities = capabilities,
+			settings = {
+				completions = {
+					completeFunctionCalls = true,
+				},
+			},
+		})
+
+		lspconfig.bashls.setup({
+			capabilities = capabilities,
+			settings = {
+				completions = {
+					completeFunctionCalls = true,
+				},
+			},
+		})
+
+		lspconfig.html.setup({
+			capabilities = capabilities,
+			settings = {
+				completions = {
+					completefunctioncalls = true,
+				},
+			},
+		})
+
+		lspconfig.cssls.setup({
+			capabilities = capabilities,
+			settings = {
+				completions = {
+					completefunctioncalls = true,
+				},
+			},
+		})
+
+		lspconfig.jsonls.setup({
+			capabilities = capabilities,
+			settings = {
+				completions = {
+					completefunctioncalls = true,
+				},
+			},
+		})
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+				local map = vim.keymap.set
+
+				map("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, silent = true, desc = "Hover" })
+				map(
+					"n",
+					"<leader>ca",
+					vim.lsp.buf.code_action,
+					{ buffer = ev.buf, silent = true, desc = "Code action" }
+				)
+				map("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Declaration", silent = true })
+				map("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, silent = true, desc = "Definition" })
+				map("n", "gi", vim.lsp.buf.implementation, { buffer = ev.buf, silent = true, desc = "Implementation" })
+				map("n", "J", vim.diagnostic.goto_next, { buffer = ev.buf, silent = true, desc = "Diagnostic" })
+				map("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, silent = true, desc = "References" })
+				vim.keymap.set("n", "<space>cf", function()
+					vim.lsp.buf.format({ async = true })
+				end, { buffer = ev.buf, silent = true, desc = "Format" })
+			end,
 		})
 	end,
 }
