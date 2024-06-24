cf90351bbef33b4301246023c433d63110aee4ed
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-06-24 00:41:55 -0300

    adding keybind to lsp rename ,action and add icons to mason

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,63 +1,67 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(_, bufnr)
-    local opts = { buffer = bufnr }
-    lsp_zero.default_keymaps(opts)
-    vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  local opts = { buffer = bufnr }
+  lsp_zero.default_keymaps(opts)
+  vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end)
 
 require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "",
-            package_pending = "➜",
-            package_uninstalled = "",
-        }
+  ui = {
+    icons = {
+      package_installed = "",
+      package_pending = "➜",
+      package_uninstalled = "",
     }
+  }
 })
 
 
 require('mason-lspconfig').setup({
-    ensure_installed = {
-        "lua_ls",
-        "tsserver",
-        "eslint",
-        "terraformls",
-        "docker_compose_language_service",
-        "dockerls",
-        "gopls",
-        "pyright",
-        "intelephense",
-        "tailwindcss",
-        "prismals",
-        "bashls",
-        "cssls",
-        "html",
-        "jsonls",
+  ensure_installed = {
+    "lua_ls",
+    "tsserver",
+    "eslint",
+    "terraformls",
+    "docker_compose_language_service",
+    "dockerls",
+    "gopls",
+    "pyright",
+    "intelephense",
+    "tailwindcss",
+    "prismals",
+    "bashls",
+    "cssls",
+    "html",
+    "jsonls",
 
-    },
-    handlers = {
-        function(server_name)
-            require('lspconfig')[server_name].setup({})
-        end,
-    },
+  },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
 })
 
 local cmp = require "cmp"
 
 cmp.setup({
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    formatting = {
-        fields = { 'abbr', 'kind', 'menu' },
-        format = require('lspkind').cmp_format({
-            mode = 'symbol',       -- show only symbol annotations
-            maxwidth = 50,         -- prevent the popup from showing more than provided characters
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-            show_labelDetails = true,
-        })
-    },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    fields = { 'abbr', 'kind', 'menu' },
+    format = require('lspkind').cmp_format({
+      mode = 'symbol',       -- show only symbol annotations
+      maxwidth = 50,         -- prevent the popup from showing more than provided characters
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+      show_labelDetails = true,
+    })
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
 })
