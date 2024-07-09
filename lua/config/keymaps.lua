dd1067af3dc9870642e45743e90b853b915082f4
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-07-09 19:21:17 -0300

    feat: new config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,44 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+vim.g.mapleader = " "
+
+map("n", "<leader>pv", vim.cmd.Ex)
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "<Esc>", "<cmd>nohl<cr>")
+
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- greatest remap ever
+map("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+
+map({ "n", "v" }, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+map("i", "<C-c>", "<Esc>")
+map("i", "<C-[>", "<Esc>")
+
+map("n", "<C-k>", "<cmd>cnext<CR>zz")
+map("n", "<C-j>", "<cmd>cprev<CR>zz")
+map("n", "<leader>k", "<cmd>lnext<CR>zz")
+map("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Format
+map("n", "<leader>f", function()
+  vim.lsp.buf.format({ timeout_ms = 2000 })
+end, { desc = "format files" })
