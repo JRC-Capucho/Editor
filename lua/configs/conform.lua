d48396de4895cf7d6d8743bc2fa63d07d13010c3
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-08-15 22:57:53 -0300

    format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,11 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = { "isort", "black" },
+    go = { "goimports", "gofumpt" },
+    terraform = { "terraform_fmt" },
+    tf = { "terraform_fmt" },
+    ["terraform-vars"] = { "terraform_fmt" },
   },
 
   format_on_save = {
