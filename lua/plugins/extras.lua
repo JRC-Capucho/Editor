bf63860def931bffb34182ec576bf3b64d7760c0
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2025-03-07 22:08:50 -0300

    extras

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,1 +1,27 @@
-return {}
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "phpstan",
+        "pint",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        php = { "pint" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        php = { "phpstan" },
+      },
+    },
+  },
+}
